006feb70cafe894fe559d7b464d8c9986cc8dda4
Dima <akmeev2011@yandex.ru>
2022-11-17 22:03:49 +0300

    Initial

---
+++ src/main/java/com/example/library_commonlang3_hw/LibraryCommonlang3HwApplication.java
@@ -0,0 +1,13 @@
+package com.example.library_commonlang3_hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryCommonlang3HwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryCommonlang3HwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/library_commonlang3_hw/LibraryCommonlang3HwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.library_commonlang3_hw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryCommonlang3HwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
